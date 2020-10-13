9b086f526c9c8f908956adc00895dbff78e1b763
UIT.19520296 <68140820+thuytien192@users.noreply.github.com>
2020-10-13 23:10:37 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuViPApp
+{
+    public partial class Form_Muvip : Form
+    {
+        public Form_Muvip()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Home_Click(object sender, EventArgs e)
+        {
+            panel_Player.Hide();
+        }
+    }
+}
