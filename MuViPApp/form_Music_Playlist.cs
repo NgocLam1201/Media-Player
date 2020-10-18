d251e7514bce03ef2acc55b27df06feb1e7df684
UIT.19520296 <68140820+thuytien192@users.noreply.github.com>
2020-10-18 15:51:06 +0700

    Add files via upload

---
+++ MuViPApp/form_Music_Playlist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuViPApp
+{
+    public partial class form_Music_Playlist : Form
+    {
+        
+        public form_Music_Playlist(string s)
+        {
+            
+            InitializeComponent();
+            lb_NamePl.Text = s;
+
+        }
+
+        private void btn_Rename_Click(object sender, EventArgs e)
+        {
+            form_Rename_pl form_Rename = new form_Rename_pl();
+            form_Rename.Show(this); 
+
+        }
+    }
+}
