d706cadd709d09ae4624aedce0aa1ba0554aa81d
Đoàn Ngọc Lãm <57290977+lamngok1201@users.noreply.github.com>
2020-10-19 07:15:31 +0700

    Ket noi sql add form login (#8)
    * Add files via upload
    
    add_class_and_update_form
    
    * Add files via upload
    
    Update_giao_dien_va add_class_account

---
+++ MuViPApp/form_Login.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuViPApp
+{
+    public partial class form_Login : Form
+    {
+        public Form parent;
+        public form_Login()
+        {
+            InitializeComponent();
+        }
+
+        public form_Login(Form parent)
+        {
+            this.parent = parent;
+            InitializeComponent();
+        }
+
+        private void btn_Login_Click(object sender, EventArgs e)
+        {
+            if (Login(tb_Login_UserName.Text, tb_Login_PassWord.Text)){}
+            else
+            {
+                MessageBox.Show("The e-mail or password that you've entered is incorrect", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private bool Login(string email, string password)
+        {
+            return Account.Instance.Login(email, password);
+        }
+
+        private void btn_Login_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lb_Login_Signup_Click(object sender, EventArgs e)
+        {
+            form_Signup frmSignUp = new form_Signup(this);
+            frmSignUp.ShowDialog();
+        }
+    }
+}
---
+++ MuViPApp/form_Signup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MuViPApp
+{
+    public partial class form_Signup : Form
+    {
+        public Form parent;
+        public form_Signup()
+        {
+            InitializeComponent();
+        }
+
+        public form_Signup(Form parent)
+        {
+            this.parent = parent;
+            InitializeComponent();
+        }
+
+        private void btn_SignUp_Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Signup_Click(object sender, EventArgs e)
+        {
+            if (SignUp(tb_SignUp_UserName.Text, tb_Signup_PassWord.Text, tb_SignUp_RePass.Text))
+            {
+                Account.Instance.SignUp(tb_SignUp_UserName.Text, tb_Signup_PassWord.Text);
+                this.Close();
+                this.parent.Show();
+            }
+        }
+
+        private bool SignUp(string email, string password, string rePass)
+        {
+            if (!Account.Instance.isLengthLargeThanEight(password))
+            {
+                ShowError("Your password must be more than 8 characters long");
+                return false;
+            }    
+            if (!Account.Instance.CheckInput(email, password))
+            {
+                ShowError("Please enter a valid email address or password");
+                return false;
+            }
+            if (!Account.Instance.CheckRePassword(password, rePass))
+            {
+                ShowError("Your password is not match. Please try again");
+                return false;
+            }
+            if (Account.Instance.isEmailExist(email))
+            {
+                ShowError("That email is already in use. Please try again");
+                return false;
+            }
+            return true;
+        }
+
+        private void ShowError(string text)
+        {
+            MessageBox.Show(text, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void txtEmail_Click(object sender, EventArgs e)
+        {
+            tb_SignUp_UserName.Clear();
+        }
+
+        private void Form_Signup_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.parent.Show();
+        }
+
+        private void Form_Signup_Load(object sender, EventArgs e)
+        {
+            this.parent.Hide();
+        }
+    }
+}
